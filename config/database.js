eb8a593e98eee743e6c1dea1b51cda19f7a9cb2a
Gerardo Lopez Duenas <gerardo.lopez@magmalabs.io>
2019-03-17 23:18:16 -0400

    Remove unnecessary DB configurarion

--- config/database.js
+++ config/database.js
@@ -39,33 +39,10 @@ module.exports = {
 
   /*
   |--------------------------------------------------------------------------
-  | MySQL
-  |--------------------------------------------------------------------------
-  |
-  | Here we define connection settings for MySQL database.
-  |
-  | npm i --save mysql
-  |
-  */
-  mysql: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
-  },
-
-  /*
-  |--------------------------------------------------------------------------
   | PostgreSQL
   |--------------------------------------------------------------------------
   |
   | Here we define connection settings for PostgreSQL database.
-  |
-  | npm i --save pg
   |
   */
   pg: {
